b9f2c7320d4c5d06181947a8c96228c61c3fd6b7
Travis Shears <btbtravis@gmail.com>
2019-03-15 22:13:28 +0100

    Init rec types

--- src/Main.purs
+++ src/Main.purs
@@ -3,7 +3,57 @@ module Main where
 import Prelude
 import Effect (Effect)
 import Effect.Console (log)
+import Data.Array((!!))
+import Data.Maybe (Maybe(..), fromMaybe)
+--- import Data.Array (length, head)
 
 main :: Effect Unit
 main = do
-  log "Hello sailor!"
+    log $ case (getRec Base) !! 0 of
+               Just x -> x
+               Nothing -> "Fail"
+    
+
+-- log $ fromMaybe "fail" (Nothing)
+-- main = logShow (diagonal 3.0 4.0)
+-- log "Hello World"
+data Rect
+    = Base
+    | BaseOffset
+    | Tree1
+    | Rocks1
+
+getRec :: Rect -> Array String
+getRec rec = 
+    case rec of
+         Base ->
+             [
+             "._._",
+             "_._.",
+             "._._",
+             "_._."
+             ]
+         BaseOffset ->
+             [
+             "_._.",
+             "._._",
+             "_._.",
+             "._._"
+             ]
+         Tree1 ->
+             [
+             "_._.",
+             "._._",
+             "_/\\_.",
+             "/..\\"
+             ]
+         Rocks1 ->
+             [
+             "_.()",
+             "._._",
+             "_._.",
+             "()._"
+             ]
+
+
+
