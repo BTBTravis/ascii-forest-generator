8d42ab92b5d79ca8c16b9d15a5415105812548a0
Travis Shears <btbtravis@gmail.com>
2019-03-15 23:37:07 +0100

    Get basic row building working

--- src/Main.purs
+++ src/Main.purs
@@ -3,23 +3,31 @@ module Main where
 import Prelude
 import Effect (Effect)
 import Effect.Console (log)
-import Data.Array((!!))
+import Data.Array((!!), zipWith, foldl, head, tail)
 import Data.Maybe (Maybe(..), fromMaybe)
 --- import Data.Array (length, head)
 
 main :: Effect Unit
 main = do
-    log $ case (getRec Base) !! 0 of
-               Just x -> x
-               Nothing -> "Fail"
+    log $ foldl (<>) "\n" $ map (\n -> n <> "\n") $ row (getRec Base) [(getRec Base), (getRec Rocks1), (getRec Base), (getRec Tree1), (getRec Base)]
+
+row :: Array String -> Array(Array String) -> Array String
+row acc [] = acc
+row acc l = row (zipWith (<>) acc (fromMaybe [] $ head l)) (fromMaybe [] $ tail l)
     
+
+-- fact 0 acc = acc
+-- fact n acc = fact (n - 1) (acc * n)
+
+combineRecs :: Array String -> Array String -> Array String
+combineRecs a1 a2 =
+    zipWith (<>) a1 a2
 
 -- log $ fromMaybe "fail" (Nothing)
 -- main = logShow (diagonal 3.0 4.0)
 -- log "Hello World"
 data Rect
     = Base
-    | BaseOffset
     | Tree1
     | Rocks1
 
@@ -27,13 +35,6 @@ getRec :: Rect -> Array String
 getRec rec = 
     case rec of
          Base ->
-             [
-             "._._",
-             "_._.",
-             "._._",
-             "_._."
-             ]
-         BaseOffset ->
              [
              "_._.",
              "._._",
