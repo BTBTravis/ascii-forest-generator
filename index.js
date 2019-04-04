53ea829f2ead3b474511dfd5a58f4dcc262aacfd
Travis Shears <btbtravis@gmail.com>
2019-04-04 23:07:57 +0200

    Add generated js to repo for use as module

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const pureForest = require('./output/Main/index.js');
+const pureForest = require('./build.js');
 
 exports.generateForest = function(x, y) {
   return pureForest.generate(Math.round(Math.random() * 10000) + 1)(x)(y);
